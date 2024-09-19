a3e4ac60c2684bd0bf6d5eb9c85048d21f437b0d
OJ Abba <phavorabba@gmail.com>
2024-09-19 10:10:02 +0100

    Add about and projects sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import { projects } from '@/data/projects'
 import { user } from '@/data/'
+import ContentWrapper from '@/components/ContentWrapper'
 import Link from 'next/link'
+import ProjectCard from '@/components/ProjectCard/'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <div className={styles.navWrapper}>
+        <nav>
+          <ul>
+            <li>
+              <Link href={'#about'}>About</Link>
+            </li>
+
+            <li>
+              <Link href={'#projects'}>Projects</Link>
+            </li>
+            <li>
+              <Link href={'mailto:oj@underonecloud.com'}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       {/* Hero section containing user details and social links */}
       <section className={styles.heroSection}>
         <div className={styles.userDetails}>
@@ -31,6 +50,28 @@ export default function Home() {
           </div>
         )}
       </section>
+
+      <ContentWrapper>
+        <section className={styles.section} id="about">
+          <h2>About Me</h2>
+
+          <div className={styles.userProfile}>
+            {user.profile.map((paragraph) => {
+              return <p key={paragraph}>{paragraph}</p>
+            })}
+          </div>
+        </section>
+
+        <section className={styles.section} id="projects">
+          <h2>Projects</h2>
+
+          <div className={styles.projectsWrapper}>
+            {projects.map((project) => {
+              return <ProjectCard key={project.title} project={project} />
+            })}
+          </div>
+        </section>
+      </ContentWrapper>
     </div>
   )
 }
