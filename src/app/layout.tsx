1e772de66f2b205cbeb2b7731cd07ee57cce646a
OJ Abba <phavorabba@gmail.com>
2024-10-09 15:04:39 +0100

    Update site to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         <Theme
           className="themeWrapper"
           accentColor="gray"
+          appearance="dark"
           hasBackground={false}
         >
           {children}
