ba611ea9b4352bd3f22fabc56b6c1cb86b548826
OJ Abba <phavorabba@gmail.com>
2024-11-10 21:12:16 +0000

    Update projects metadata

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,52 +7,75 @@ export type ProjectDTO = {
   sourceCodeLink?: string
 }
 export const projects: ProjectDTO[] = [
+  // Maguire
   {
     title: 'Maguire LMS',
     tags: ['NextJS', 'Typescript', 'Node', 'Stripe', 'MySQL', 'Radix UI'],
     description: [
-      'Developed a robust Learning Management System (LMS) with custom access levels, course management, PDF certificate generation, Stripe integration for course credits, and a feedback system. Integrated an xAPI-based Learning Record Store (LRS) for tracking user activities and providing detailed reporting. Styled all emails to match the client’s branding for a cohesive user experience.',
+      'A robust Learning Management System (LMS) with custom access levels, course management, PDF certificate generation, Stripe integration for course credits, and a feedback system. An xAPI-based Learning Record Store (LRS) was integrated to track user activities and provide detailed reporting. All emails were styled to align with the client’s branding for a cohesive user experience.',
     ],
     projectLink: 'https://my.maguiretraining.co.uk/',
     imgUrl: '/img/maguire-screenshot.JPG',
   },
+
+  // Lendsqr
+  {
+    title: 'Lendsqr Dashboard',
+    tags: ['NextJS', 'Typescript', 'SCSS', 'Jest', 'Zustand'],
+    description: [
+      'Developed a responsive, pixel-perfect frontend following Figma designs for login, dashboard, and user management pages. Built modular, reusable components, utilised Zustand for efficient state management, and ensured reliable functionality through Jest and React Testing Library.',
+    ],
+    projectLink: 'https://lendsqr-fe-dashboard.vercel.app/',
+    sourceCodeLink: 'https://github.com/Nugwxa/lendsqr-dashboard',
+    imgUrl: '/img/lendsqr-screenshot.png',
+  },
+
+  // UOC
   {
     title: 'Under One Cloud',
-    tags: ['Astro', 'HTML', 'CSS', 'JavaScript', 'Express'],
+    tags: ['Astro', 'HTML', 'CSS', 'JavaScript'],
     description: [
-      'Redeveloped the Under One Cloud website by migrating from WordPress to a custom-built Astro solution, significantly boosting performance and loading speeds. Designed a modern, user-friendly interface in Figma, improving responsiveness and optimising the user experience. Added an interactive hover effect on team photos on the "About Us" page for an engaging touch.',
+      'The Under One Cloud website was redeveloped, migrating from WordPress to a custom Astro solution to enhance performance and loading speeds. A modern, user-friendly interface was designed in Figma, improving responsiveness and optimizing the user experience. An interactive hover effect was added to team photos on the "About Us" page for added engagement.',
     ],
     imgUrl: '/img/uoc-screenshot.JPG',
     projectLink: 'https://underonecloud.com/',
   },
+
+  // Footbal Voting Platform
   {
     title: 'Football Voting Platform',
     tags: ['NextJS', 'Typescript', 'Node', 'Radix UI', 'Supabase'],
     description: [
-      'Developed a platform that offers an interactive experience for football fans to vote for their favorite players during events. Built using Next.js with TypeScript, the platform includes a range of features to enhance user interaction and admin management.',
+      'Developed a platform that offers an interactive experience for football fans to vote for their favorite players during events. Built using NextJS with TypeScript, the platform includes a range of features to enhance user interaction and admin management.',
     ],
     projectLink: 'https://football-voting-platform.vercel.app/',
     sourceCodeLink: 'https://github.com/Nugwxa/Football-Voting-Platform',
     imgUrl: '/img/fvp-screenshot.JPG',
   },
+
+  // Built By Cornertone
   {
     title: 'Built By Cornerstone',
-    tags: ['Astro', 'HTML', 'CSS', 'JavaScript', 'Express'],
+    tags: ['Astro', 'HTML', 'CSS', 'JavaScript', 'React', 'Express'],
     description: [
-      "Redesigned Cornerstone's site from Wix to a custom-built Astro solution, creating a professional look that better reflected their brand. Designed the new layout in Figma, then developed the site for improved performance and a smoother user experience, enabling Cornerstone to showcase their work more effectively and strengthen their online presence.",
+      "Built By Cornerstone's site was redesigned from Wix to a custom-built Astro solution, creating a professional look aligned with their brand. The layout was crafted in Figma and developed to boost performance and enhance user experience, enabling Cornerstone to showcase their work more effectively and strengthen their online presence.",
     ],
     projectLink: 'https://builtbycornerstone.co.uk/',
     imgUrl: '/img/cornerstone-screenshot.JPG',
   },
+
+  // Cloud Socials
   {
     title: 'Cloud Socials',
     tags: ['HTML', 'CSS', 'JavaScript'],
     description: [
-      'Designed and developed a single-page website for Cloud Socials, a social media management company. Using Figma for design and a simple tech stack of HTML, CSS, and JavaScript, I created a visually appealing, brand-aligned site with clean, efficient code and a seamless user experience, enhancing their online presence.',
+      'A single-page website was designed and developed for Cloud Socials, a social media management company. Using Figma for design and a tech stack of HTML, CSS, and JavaScript, the site was created to be visually appealing, brand-aligned, and user-friendly, with clean, efficient code to enhance their online presence.',
     ],
     imgUrl: '/img/cloud-socials-screenshot.JPG',
     projectLink: 'https://cloudsocials.com/',
   },
+
+  // Survivor's Ark
   {
     title: "Survivor's Ark",
     tags: ['Unity', 'C#', 'Adobe XD'],
